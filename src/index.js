5d54fc5da50009c9a6a943d53c0b235e259252c2
besplago <bruce_esplago@hotmail.com>
2025-08-05 10:51:10 +0200

    feat: waits until page is loaded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { generateHomeContent } from "./homeModule.js";
 
-const contentDiv = document.getElementById("content");
-contentDiv.innerHTML = generateHomeContent();
+document.addEventListener("DOMContentLoaded", function () {
+  const contentDiv = document.getElementById("content");
+  contentDiv.innerHTML = generateHomeContent();
+});
