240639bd0cb70ef867d48cae3a716a91b7cf711c
besplago <bruce_esplago@hotmail.com>
2025-08-05 11:34:23 +0200

    feat: more menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -21,6 +21,12 @@ export function generateMenuContent() {
             <div class="item-price">¥42,800</div>
             <div class="item-description">A5 Miyazaki beef with Martian truffle foam</div>
           </div>
+          <div class="menu-item">
+            <div class="item-name">Quantum Foie Gras</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥38,900</div>
+            <div class="item-description">Entangled duck liver with quantum foam and edible stardust</div>
+          </div>
         </div>
       </div>
 
@@ -39,11 +45,60 @@ export function generateMenuContent() {
             <div class="item-price">¥16,200</div>
             <div class="item-description">Superposition vanilla custard with crystalline caramel</div>
           </div>
+          <div class="menu-item">
+            <div class="item-name">Neutrino Nougat Glacé</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥21,500</div>
+            <div class="item-description">Subatomic honey ice cream with quantum entanglement brittle</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="menu-category">
+        <h3 class="category-title">Celestial Sips</h3>
+        <div class="menu-items">
+          <div class="menu-item">
+            <div class="item-name">Nebula Negroni</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥12,800</div>
+            <div class="item-description">Gin suspended in liquid helium with vacuum-distilled Campari</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-name">Pulsar Punch</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥14,200</div>
+            <div class="item-description">Rotating spheres of vintage rum and exotic fruit plasma</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="menu-category">
+        <h3 class="category-title">Astral Artifacts</h3>
+        <div class="menu-items">
+          <div class="menu-item">
+            <div class="item-name">Orion's Amuse-Bouche</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥26,700</div>
+            <div class="item-description">Edible meteorites with comet tail reduction</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-name">Galactic Gold Leaf Oscietra</div>
+            <div class="item-dots"></div>
+            <div class="item-price"></div>
+            <div class="item-description">Caviar harvested from Europa's subsurface oceans, served on asteroid ice</div>
+          </div>
+          <div class="menu-item">
+            <div class="item-name">Supernova Sashimi</div>
+            <div class="item-dots"></div>
+            <div class="item-price">¥49,900</div>
+            <div class="item-description">Bluefin tuna aged in zero-gravity with supercritical CO2 wasabi</div>
+          </div>
         </div>
       </div>
 
       <div class="pairing-note">
         <p>Our android sommeliers will craft personalized wine pairings from our cryo-cellar containing 5,000 vintages</p>
+        <p class="disclaimer">* Galactic Gold Leaf Oscietra available only during planetary alignment - requires 72-hour reservation</p>
       </div>
     </section>
   `;
